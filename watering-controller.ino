bb07d15cfeb2c277705dd363b3b91e0e3e899a36
Timur Konic <timur.konic@gmail.com>
2021-09-29 20:35:25 +0300

    HTTP request parsing

--- watering-controller.ino
+++ watering-controller.ino
@@ -5,9 +5,18 @@
 #include <SPI.h>
 #include <Ethernet.h>
 
-byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED}; // MAC address of the NIC ethernet controller
-IPAddress ip(192, 168, 2, 111); // IP address of the controller
-EthernetServer server(80); // IP port of server, 80 is default for HTTP 
+// MAC address of the NIC ethernet controller
+byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
+
+// IP settings
+byte ip[] = {192, 168, 2, 111};
+byte gateway[] = {192, 168, 2, 1};
+byte subnet[] = {255, 255, 255, 0};
+
+// TCP port of server, 80 is default for HTTP 
+int tcpPort = 80;
+
+EthernetServer server = EthernetServer(tcpPort);
 
 void setup() {
   Serial.begin(9600);
@@ -16,7 +25,7 @@ void setup() {
   }
   Serial.println("Watering controller");
 
-  Ethernet.begin(mac, ip);
+  Ethernet.begin(mac, ip, gateway, subnet);
   if (Ethernet.hardwareStatus() == EthernetNoHardware) {
     Serial.println("Ethernet shield was not found");
     while (true) {
@@ -29,8 +38,60 @@ void setup() {
 
   server.begin();
   Serial.print("Controller is at ");
-  Serial.println(Ethernet.localIP());
+  Serial.print(Ethernet.localIP());
+  Serial.print(":");
+  Serial.println(tcpPort);
+}
+
+void parseURI(String uri) {
+  Serial.print("GET ");
+  Serial.println(uri);
+}
+
+void parseRequest(String line) {
+  if (line.startsWith("GET ")) {
+    int spaceIndex = line.indexOf(' ', 4);
+    if (spaceIndex > 0) {
+      String uri = line.substring(4, spaceIndex);
+      parseURI(uri);
+    }
+  }
+}
+
+void sendResponse(EthernetClient client) {
+  client.println("HTTP/1.1 200 OK");
+  client.println("Content-Type: text/html");
+  client.println("Connection: close");
+  client.println();
+  client.println("OK");
+  delay(1);
+  client.stop();
 }
 
 void loop() {
+  EthernetClient client = server.available();
+  String requestLine = "";
+  if (client) {
+    boolean requestLineIsBlank = true;
+    while (client.connected()) {
+      if (client.available()) {
+        char requestChar = client.read();
+        if (requestChar == '\n' && requestLineIsBlank) {
+          sendResponse(client);
+          break;
+        }
+        if (requestChar == '\n') {
+          requestLineIsBlank = true;
+          parseRequest(requestLine);
+          requestLine = "";
+        }
+        else {
+          if (requestChar != '\r') {
+            requestLineIsBlank = false;
+            requestLine += requestChar;
+          }
+        }
+      }
+    }
+  }
 }
